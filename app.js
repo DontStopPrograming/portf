3c415c43937aa812a3a5ccfd8584227e0849d5c2
DontStopPrograming <01dyamond01@gmail.com>
2023-01-02 15:20:18 -0500

    mod

--- app.js
+++ app.js
@@ -54,11 +54,8 @@ app.post('/register', (req, res) => {
 
     user.save(err => {
         if(err){
-            // res.status(500).send('ERROR TO REGISTER')
-           res.render('register')
+           res.status(500).send('ERROR TO REGISTER')
            
-            
-            
         } else {
             res.status(200).send('REGISTERED USER')
             
