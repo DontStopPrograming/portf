f1a3530760bc9c97621723c3c414c05bed5560e7
DontStopPrograming <01dyamond01@gmail.com>
2023-01-04 14:55:27 -0500

    modif

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/signup', (req, res) => {
     user.save(err => {
         if(err){
         //   res.status(500).send('ERROR TO REGISTER')
-            res.render('registerError')
+            res.render('signup')
         
            
         } else {
